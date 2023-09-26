3a78a9b973dc6d1566ccc77de3c3efc8c2f3f6c8
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-09-26 10:14:50 +0100

    Some frontend configurations

---
+++ src/Pages/SignIn/signin.jsx
@@ -0,0 +1,9 @@
+function SignIn(){
+	return(
+		<div>
+			<h1>I'm a sign up page</h1>
+		</div>
+	);
+}
+
+export default SignIn;
---
+++ src/Pages/SignUp/signup.jsx
@@ -0,0 +1,9 @@
+function SignUp(){
+	return(
+		<div>
+			<h1>I'm a signup page</h1>
+		</div>
+	);
+}
+
+export default SignUp;
